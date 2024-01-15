dde8d6b3a0363491a328da4b0b97b112ec90ddcc
varcoder <bhat.varun50@gmail.com>
2024-01-15 14:52:38 -0500

    changed ThriftyEncoder per https://github.com/BroncBotz3481/YAGSL/blob/main/swervelib/encoders/AnalogAbsoluteEncoderSwerve.java

--- src/main/java/frc/robot/sensors/ThriftyEncoder.java
+++ src/main/java/frc/robot/sensors/ThriftyEncoder.java
@@ -3,31 +3,33 @@ package frc.robot.sensors;
 import java.util.function.*;
 
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.AnalogInput;
-import frc.robot.misc.Constants;
+import edu.wpi.first.wpilibj.RobotController;
 
 public class ThriftyEncoder implements Supplier<Rotation2d> {
-    private final AnalogInput input;
-    private final double read_voltage_max;
+    private final AnalogInput encoder;
+    private boolean inverted = false;
     public Rotation2d offset = new Rotation2d(0.0);
 
-    public ThriftyEncoder(AnalogInput input) {
-        this(input, Constants.Swerve.thriftyMaxVoltage);
+    public ThriftyEncoder(AnalogInput encoder) {
+        this.encoder = encoder;
     }
 
-    public ThriftyEncoder(AnalogInput input, double read_voltage_max) {
-        this.input = input;
-        this.read_voltage_max = read_voltage_max;
+    public void configure(boolean inverted){ //TODO: Figure out if any of the encoders are inverted
+        this.inverted = inverted;
     }
-
     // Does not include offset
-    public Rotation2d getRawPosition() {
-        return new Rotation2d(
-            (input.getVoltage() * 2 * Math.PI) / read_voltage_max
-        );
+    private double getRawPositionHelper() {
+        return (inverted ? -1.0 : 1.0) * (encoder.getAverageVoltage() / RobotController.getVoltage5V()) * 360;
+        // return new Rotation2d(
+        //     (encoder.getVoltage() * 2 * Math.PI) / read_voltage_max
+        // );
     }
 
-
+    public Rotation2d getRawPosition(){
+        return new Rotation2d(Units.degreesToRadians(getRawPositionHelper()));
+    }
     // Does include offset
     public Rotation2d get() {
         return getRawPosition().plus(offset);
